73b8d63262b2908062568fa0ecfc765b709c38b2
Ali Ahmadi <mrkiller1383@gmail.com>
2023-07-15 13:43:24 +0330

    Scaled the cards on hover and applied transition to it

--- src/components/CardLayout.tsx
+++ src/components/CardLayout.tsx
@@ -7,7 +7,13 @@ interface Props {
 
 export default function CardLayout({ children }: Props) {
   return (
-    <Card boxShadow={"0 0 7px rgba(0,0,0,0.6)"} border={10} overflow={"hidden"}>
+    <Card
+      transition={"ease 0.2s"}
+      _hover={{ transform: "scale(1.02)" }}
+      boxShadow={"0 0 7px rgba(0,0,0,0.6)"}
+      border={10}
+      overflow={"hidden"}
+    >
       {children}
     </Card>
   );
