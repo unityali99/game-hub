83163a87c3424c8fdedb2811007a4e22b1d9ed86
Ali Ahmadi <mrkiller1383@gmail.com>
2023-07-18 11:15:57 +0330

    Added a placeholder image for games that do not have image

--- src/utils/cropImageUrl.ts
+++ src/utils/cropImageUrl.ts
@@ -1,5 +1,5 @@
 const cropImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return "https://placehold.co/600x400";
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
