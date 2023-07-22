a077ed111e3fcf425f406c168fbf3113c11c8faa
Ali Ahmadi <mrkiller1383@gmail.com>
2023-07-22 13:49:15 +0330

    Fixed the problem with menu Buttons in both dark mode on and off

--- src/utils/MenuButtonStyles.ts
+++ src/utils/MenuButtonStyles.ts
@@ -2,7 +2,8 @@ import { BsChevronDown } from "react-icons/bs";
 import { IconType } from "react-icons/lib/esm/iconBase";
 
 export const menuButtonStyle = {
-  bgColor: "gray.700",
+  border: "1px solid",
+  borderColor: "gray.700",
   px: 3.5,
   py: 2,
   rounded: 10,
@@ -10,5 +11,5 @@ export const menuButtonStyle = {
 export const menuButtonIconStyle = {
   fontSize: 10,
   as: BsChevronDown as IconType,
-  mx: 2,
+  mx: 1.5,
 };
